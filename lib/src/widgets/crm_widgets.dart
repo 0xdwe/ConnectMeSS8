3203f09c58b41d2e9d5fb39c0e4e42ae4eabe28f
Cliff Owen <cliffow29@gmail.com>
2026-06-14 16:32:44 +0800

    feat(ui): use transparent PNG for DailyNudgeCard character image

--- lib/src/widgets/crm_widgets.dart
+++ lib/src/widgets/crm_widgets.dart
@@ -1347,27 +1347,11 @@ class DailyNudgeCard extends StatelessWidget {
                 ],
               ),
             ),
-            Container(
+            SizedBox(
               width: 110,
               height: 110,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                shape: BoxShape.circle,
-                border: Border.all(
-                  color: tokens.border,
-                  width: 1.5,
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withValues(alpha: 0.05),
-                    blurRadius: 10,
-                    offset: const Offset(0, 4),
-                  ),
-                ],
-              ),
-              clipBehavior: Clip.antiAlias,
               child: Image.asset(
-                'assets/images/nudge_character.jpg',
+                'assets/images/nudge_character.png',
                 fit: BoxFit.contain,
               ),
             ),
